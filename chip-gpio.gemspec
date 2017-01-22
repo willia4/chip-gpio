7b566a581ece06f5e348bcc9f20763ebd6481dd6
James Williams <james@jameswilliams.me>
2017-01-22 14:52:23 +0000

    Add interrupt support to GPIO pins

--- chip-gpio.gemspec
+++ chip-gpio.gemspec
@@ -1,8 +1,8 @@
 
 Gem::Specification.new do |s|
   s.name        = 'chip-gpio'
-  s.version     = '0.1.2'
-  s.date        = '2017-01-19'
+  s.version     = '0.2.0'
+  s.date        = '2017-01-22'
   s.homepage    = 'http://github.com/willia4/chip-gpio'
   s.summary     = "A ruby gem to control the IO hardware on the CHIP computer"
   s.description = "A ruby gem to control the IO hardware the CHIP computer"
@@ -10,4 +10,6 @@ Gem::Specification.new do |s|
   s.email       = 'james@jameswilliams.me'
   s.license     = 'MIT'
   s.files       = Dir.glob("{lib}/**/*") + %w(README.md)
+
+  s.add_runtime_dependency 'epoll'
 end
--- lib/chip-gpio/Pin.rb
+++ lib/chip-gpio/Pin.rb
@@ -1,11 +1,24 @@
+require 'epoll'
+
 module ChipGPIO
   module Pin
     class Pin
       attr_reader :gpio_number
+      attr_reader :pin_name
 
-      def initialize(gpio_number)
+      def initialize(gpio_number, pin_name)
         @gpio_number = gpio_number
+        @pin_name = pin_name
         @base_path = "/sys/class/gpio/gpio#{@gpio_number}"
+
+        @interrupt_thread = nil
+        @stop_waiting = false 
+        @stop_waiting_lock = Mutex.new 
+
+        @interrupt_procs = []
+        @interrupt_procs_lock = Mutex.new
+
+        @interrupt_pipe_read, @interrupt_pipe_write = IO.pipe()
       end
 
       def available?
@@ -45,6 +58,52 @@ module ChipGPIO
         File.open("#{@base_path}/direction", "w") { |f| f.write(v) }
       end
 
+      def edge
+        v = File.read("#{@base_path}/edge").strip
+        case v
+        when "both"
+          return :both
+        when "rising"
+          return :rising
+        when "falling"
+          return :falling
+        else
+          throw "Unexpected edge #{v}"
+        end
+      end
+
+      def edge=(e)
+        case e
+        when :both
+          v = "both"
+        when :rising
+          v = "rising"
+        when :falling
+          v = "falling"
+        else
+          throw "Unexpected edge: #{e}; must be :both, :rising, or :falling"
+        end
+
+        File.open("#{@base_path}/edge", "w") { |f| f.write(v) }
+      end
+
+      def active_low
+        v = File.read("#{@base_path}/active_low").strip
+        case v
+        when "0"
+          return false
+        when "1"
+          return true
+        else
+          throw "Unexpectd active_low: #{v}"
+        end
+      end
+
+      def active_low=(b)
+        v = !!b ? "1" : "0"
+        File.open("#{@base_path}/active_low", "w") { |f| f.write(v) }
+      end
+
       def value
         throw "Pin is not currently available" if !self.available?
 
@@ -63,8 +122,58 @@ module ChipGPIO
         v = v.to_s
         File.open("#{@base_path}/value", "w") { |f| f.write(v) }
       end
-    end
 
+      def on_interrupt(&proc)
+        return if !proc 
+        throw "Interrupt is only valid for input pins" if direction != :input
+        
+        @interrupt_procs_lock.synchronize { @interrupt_procs << proc  } 
+          
+        if !@interrupt_thread
+          @interrupt_thread = Thread.new("#{@base_path}/value") do |pin_path|
+            
+            fd = open(pin_path, 'r')
+
+            epoll = Epoll.create()
+            epoll.add(fd, Epoll::PRI)
+            epoll.add(@interrupt_pipe_read, Epoll::IN)
+
+            #read the value once before polling 
+            fd.seek(0)
+            fd.read()
+
+            while true 
+              stop = false 
+              @stop_waiting_lock.synchronize { stop = @stop_waiting }
+              break if stop 
+
+              evlist = epoll.wait()
+
+              evlist.each do |ev|
+
+                if ev.data.fileno == fd.fileno 
+                  ev.data.seek(0)
+                  value = ev.data.read().delete("\n")
+
+                  procs = []
+                  @interrupt_procs_lock.synchronize { procs = @interrupt_procs.dup() }    
+
+                  procs.each { |p| p.call(value) }
+                end
+              end
+            end
+          end
+        end
+      end
+
+      def cancel_interrupt()
+        @stop_waiting_lock.synchronize { @stop_waiting = true  } 
+        @interrupt_pipe_write.write("0")
+        @interrupt_thread.join()
+        @interrupt_thread = nil
+      end
+
+    end
   end
 
   def self.get_xio_base
@@ -91,8 +200,8 @@ module ChipGPIO
       
       pins = {}
 
-      cis.each_with_index { |gpio, index| pins["CSI#{index}".to_sym] = Pin::Pin.new(gpio) }
-      xio.each_with_index { |gpio, index| pins["XIO#{index}".to_sym] = Pin::Pin.new(gpio) }
+      cis.each_with_index { |gpio, index| sym = "CSI#{index}".to_sym; pins[sym] = Pin::Pin.new(gpio, sym) }
+      xio.each_with_index { |gpio, index| sym = "XIO#{index}".to_sym; pins[sym] = Pin::Pin.new(gpio, sym) }
 
       return pins
   end
